f5faf0e237a61ffa8257076170f1ab4d01b780f2
Braian-Zignago <braianzig77@gmail.com>
2025-05-06 21:25:49 -0300

    Initial commit

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,40 @@
+package me.dio.domain.model;
+
+public class Account {
+    private String number;
+    private String agency;
+    private double balance;
+    private double limit;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+}
---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,22 @@
+package me.dio.domain.model;
+
+public class Card {
+    private String number;
+    private double limit;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+}
